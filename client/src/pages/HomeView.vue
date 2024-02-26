87a3e490a793863f8b4758fe7e3a7f8c042ae484
Bumilib <103141505+Bumilib@users.noreply.github.com>
2024-02-26 14:59:34 -0500

    some wrong changes

--- client/src/pages/HomeView.vue
+++ client/src/pages/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
+
 </script>
 
 <template>
-  <main>
+  
+  <main> class ""
     <TheWelcome />
   </main>
 </template>
